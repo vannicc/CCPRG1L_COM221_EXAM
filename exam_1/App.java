0ac7872e65c50f15341b04148e594474f89c6b78
Jerico Lim <111488202+vannicc@users.noreply.github.com>
2022-09-08 14:22:08 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+       
+    Student info = new Student("Lim", "Jerico", 'C', "December 18, 2003", 2022102256, "limjc@students.national-u.edu.ph", true);
+        info.sayMyStudentNumber();
+        info.sayMyEmailAddress();
+        info.amIAwesome();
+    }
+}
